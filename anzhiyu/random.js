22ad1bfb1b292a3d0c4827456fe5f2db88724f67
CY <ycustc@mail.ustc.edu.cn>
2025-07-29 22:38:05 +0800

    Site updated: 2025-07-29 22:38:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/29/hello-world/","2025/07/29/hello-world2/","2025/07/29/hello-world2 - 副本/"];function toRandomPost(){
+var posts=["2025/07/29/hello-world/","2025/07/29/hello-world2 - 副本/","2025/07/29/hello-world2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
